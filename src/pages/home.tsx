fdb7e0a03661bdc6a2f0167f3cec41230f4cda70
Kevin <kevinsetiawan2412@gmail.com>
2025-10-21 21:14:15 +0700

    KELAR NIH MALEM RAHHHHHHHHHHHHHHHHHHH

---
+++ src/pages/home.tsx
@@ -0,0 +1,16 @@
+import { Col, Layout, Row, Typography } from "antd"
+import { Header } from "antd/es/layout/layout"
+
+const { Text } = Typography
+
+const Home = () => {
+    <Layout>
+        <Header>
+            <Col>
+                <Row>
+                    <Text></Text>
+                </Row>
+            </Col>
+        </Header>
+    </Layout>
+}
